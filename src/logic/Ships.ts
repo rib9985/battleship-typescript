665d84a8392f1246248cd57ed93ed3d8e343092b
Rafael Ribeiro <rib9985@gmail.com>
2026-02-13 10:37:05 -0800

    Extract Fleet map to separate class, change public Gameboard methods
    Refactor and extract the fleet map into a separate class, moving the
    class into the Ships module
    
    Extract Fleet map to separate class, change public Gameboard methods
    
    Refactor and extract the fleet map into a separate class, moving the
    class into the Ships module
    
    Extract Fleet map to separate class, change public Gameboard methods
    
    Refactor and extract the fleet map into a separate class, moving the
    class into the Ships module
    
    Extract Fleet map to separate class, change public Gameboard methods
    
    Refactor and extract the fleet map into a separate class, moving the
    class into the Ships module

--- src/logic/Ships.ts
+++ src/logic/Ships.ts
@@ -1,14 +1,20 @@
-export default class Ship {
-	shipClass: string
-	length: number
-	hits: number
-	sunk: boolean
+type shipClass = string
+type length = number
+type hits = number
+type sunk = boolean
+type shipMap = Map<string, Ship>
 
-	constructor(shipClass: string, length: number, hits = 0, sunk = false) {
+export class Ship {
+	shipClass: shipClass
+	length: length
+	hits: hits
+	sunk: sunk
+
+	constructor(shipClass: string, length: number, hits = 0) {
 		this.length = length;
 		this.shipClass = shipClass;
 		this.hits = hits;
-		this.sunk = sunk;
+		this.sunk = this.isSunk();
 	}
 
 	isSunk(): boolean {
@@ -30,20 +36,45 @@ export default class Ship {
 	}
 }
 
-const carrier = new Ship("carrier", 4);
-const battleship = new Ship("battleship", 3);
-const cruiser = new Ship("cruiser", 2);
-const submarine = new Ship("submarine", 1);
+export class Fleet {
+	ships: shipMap
 
-const fleet = [
-	carrier,
-	battleship,
-	battleship,
-	cruiser,
-	cruiser,
-	cruiser,
-	submarine,
-	submarine,
-	submarine,
-	submarine,
-];
+	constructor() {
+		this.ships = this.createFleet()
+	}
+
+	private createFleet(): shipMap {
+		const carrier = new Ship("carrier", 4);
+		const battleshipOne = new Ship("battleshipOne", 3);
+		const battleshipTwo = new Ship("battleshipTwo", 3);
+		const cruiserOne = new Ship("cruiserOne", 2);
+		const cruiserTwo = new Ship("cruiserTwo", 2);
+		const cruiserThree = new Ship("cruiserThree", 2);
+		const submarineOne = new Ship("submarineOne", 1);
+		const submarineTwo = new Ship("submarineTwo", 1);
+		const submarineThree = new Ship("submarineThree", 1);
+		const submarineFour = new Ship("submarineFour", 1);
+
+		const fleetArr = [
+			carrier,
+			battleshipOne,
+			battleshipTwo,
+			cruiserOne,
+			cruiserTwo,
+			cruiserThree,
+			submarineOne,
+			submarineTwo,
+			submarineThree,
+			submarineFour,
+		];
+		const fleetMap = new Map()
+		fleetArr.forEach(element => fleetMap.set(element.shipClass, element))
+		return fleetMap;
+	}
+
+	returnShip(shipClass: string): Ship | null {
+		const ship = this.ships.get(shipClass)
+		return ship ? ship : null
+	}
+
+}
